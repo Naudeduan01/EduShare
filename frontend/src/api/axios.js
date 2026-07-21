8d53d855352439afeb1b2fdad219ac7cdceb11ff
Eduan <naudeduan01@gmail.com>
2026-07-20 23:10:34 -0300

    Configura front para API AWS

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://5p48xt1yd0.execute-api.us-east-1.amazonaws.com/"
 });
 api.interceptors.request.use(
     config => {
